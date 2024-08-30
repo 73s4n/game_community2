aaeda23be0ca25e74e46587fdaef5f04bffc162f
YuyaSannami <155421775+73s4n@users.noreply.github.com>
2024-08-30 17:04:12 +0000

    Initial Commit]

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  Admin.create!(
-    password: ## 任意のパスワード
+  Admin.create!(email: "#{ENV["admin@example.com"]}", 
+    password: "#{ENV["password"]}", 
+    password_confirmation: "#{ENV["password"]}"
   )
