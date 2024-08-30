dfd1be5ea3f9030f6048adb9e36fa81ab6e3a630
YuyaSannami <155421775+73s4n@users.noreply.github.com>
2024-08-30 10:32:01 +0000

    Initial Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_30_101043) do
+ActiveRecord::Schema.define(version: 2024_08_03_004404) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,23 +52,6 @@ ActiveRecord::Schema.define(version: 2024_08_30_101043) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "group_users", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "group__id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["group__id"], name: "index_group_users_on_group__id"
-    t.index ["user_id"], name: "index_group_users_on_user_id"
-  end
-
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.text "introduction"
-    t.integer "owner_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -101,6 +84,4 @@ ActiveRecord::Schema.define(version: 2024_08_30_101043) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "group_users", "group_s"
-  add_foreign_key "group_users", "users"
 end
